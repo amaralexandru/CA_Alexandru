4e65d32582097c72775ec1dc95dc784d5eb566ab
CA <amar.alexandru@gmail.com>
2022-08-27 12:54:20 +0100

    5th

--- enquiry.php
+++ enquiry.php
@@ -1,4 +1,34 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+if(isset($_POST['submit1']))
+{
+$fname=$_POST['fname'];
+$email=$_POST['email'];	
+$mobile=$_POST['mobileno'];
+$subject=$_POST['subject'];	
+$description=$_POST['description'];
+$sql="INSERT INTO  tblenquiry(FullName,EmailId,MobileNumber,Subject,Description) VALUES(:fname,:email,:mobile,:subject,:description)";
+$query = $dbh->prepare($sql);
+$query->bindParam(':fname',$fname,PDO::PARAM_STR);
+$query->bindParam(':email',$email,PDO::PARAM_STR);
+$query->bindParam(':mobile',$mobile,PDO::PARAM_STR);
+$query->bindParam(':subject',$subject,PDO::PARAM_STR);
+$query->bindParam(':description',$description,PDO::PARAM_STR);
+$query->execute();
+$lastInsertId = $dbh->lastInsertId();
+if($lastInsertId)
+{
+$msg="Enquiry  Successfully submited";
+}
+else 
+{
+$error="Something went wrong. Please try again";
+}
 
+}
+?>
 <!DOCTYPE HTML>
 <html>
 <head>
